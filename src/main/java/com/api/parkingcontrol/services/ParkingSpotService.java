d691b80174d21371dc806556cc92df495c51c643
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-09-25 19:22:35 -0300

    contrutor para injeção de dependencia

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,8 +1,19 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParkingSpotService {
+
+    @Autowired
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+
+    }
+
 
 }
