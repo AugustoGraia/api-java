b31b0124d50164fcdb50685562390d20ec8f587b
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-09-29 10:34:53 -0300

    modelProduct

--- src/main/java/com/api/parkingcontrol/models/ProductModel.java
+++ src/main/java/com/api/parkingcontrol/models/ProductModel.java
@@ -1,4 +1,59 @@
 package com.api.parkingcontrol.models;
 
-public class ProductModel {
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "ProductModel")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nameProduct;
+
+    @Column(nullable = false, unique = false, length = 80)
+    private String validProduct;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public String getValidProduct() {
+        return validProduct;
+    }
+
+    public void setValidProduct(String validProduct) {
+        this.validProduct = validProduct;
+    }
+
+    public BigDecimal getValueProduct() {
+        return valueProduct;
+    }
+
+    public void setValueProduct(BigDecimal valueProduct) {
+        this.valueProduct = valueProduct;
+    }
+
+    @Column(nullable = false)
+    private BigDecimal valueProduct;
+
 }
