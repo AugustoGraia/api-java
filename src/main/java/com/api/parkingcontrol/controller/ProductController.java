6abbe5cb69e507aafad78369b0c6750993f77aa0
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-09-29 11:07:31 -0300

    ControllerProduct

--- src/main/java/com/api/parkingcontrol/controller/ProductController.java
+++ src/main/java/com/api/parkingcontrol/controller/ProductController.java
@@ -1,10 +1,23 @@
 package com.api.parkingcontrol.controller;
 
+import com.api.parkingcontrol.services.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
     @RestController
     @CrossOrigin(origins = "*", maxAge = 3600)
     public class ProductController {
 
+        final ProductService productService;
+
+        public ProductController(ProductService productService) {
+            this.productService = productService;
+        }
+
+        @PostMapping("salve/product")
+        public ResponseEntity<Object> saveProduct(){
+            return
+        }
     }
