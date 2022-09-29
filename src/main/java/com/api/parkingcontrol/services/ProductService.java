c3622453e4a58e72c855c18faad33df8af3a99f4
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-09-29 11:04:06 -0300

    controller

--- src/main/java/com/api/parkingcontrol/services/ProductService.java
+++ src/main/java/com/api/parkingcontrol/services/ProductService.java
@@ -1,5 +1,22 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.models.ProductModel;
+import com.api.parkingcontrol.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
 public class ProductService {
 
+    final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public ProductModel save(ProductModel productModel){
+        return ProductRepository.save(productModel);
+    }
 }
