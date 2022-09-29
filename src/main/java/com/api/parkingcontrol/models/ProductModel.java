0a0cc4f8080457701a00ce44dbc2afdc737e959e
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-09-29 10:49:39 -0300

    Service/Repository

--- src/main/java/com/api/parkingcontrol/models/ProductModel.java
+++ src/main/java/com/api/parkingcontrol/models/ProductModel.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Table(name = "ProductModel")
 public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
