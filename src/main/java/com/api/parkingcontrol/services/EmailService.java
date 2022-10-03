45383ad3cd752ef14a3cf97236335d9787e71efc
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-10-03 09:03:31 -0300

    emailService.enviarEmail

--- src/main/java/com/api/parkingcontrol/services/EmailService.java
+++ src/main/java/com/api/parkingcontrol/services/EmailService.java
@@ -1,21 +1,44 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.enums.StatusEmail;
 import com.api.parkingcontrol.models.EmailModel;
 import com.api.parkingcontrol.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
 
     @Autowired
-   final EmailRepository emailRepository;
+    final EmailRepository emailRepository;
 
     public EmailService(EmailRepository emailRepository) {
         this.emailRepository = emailRepository;
     }
 
-    public void enviarEmail(EmailModel emailModel) {
+    @Autowired
+    private JavaMailSender emailSender;
 
+    public EmailModel enviarEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
     }
 }
