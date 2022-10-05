f15a8e5b1641b7e1e4640b97475b787afd80fa26
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-10-05 15:10:36 -0300

    getListaId

--- src/main/java/com/api/parkingcontrol/controller/ProductController.java
+++ src/main/java/com/api/parkingcontrol/controller/ProductController.java
@@ -49,7 +49,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
                 return new ResponseEntity<List<ProductModel>>(productList, HttpStatus.OK);
         }
     }
-
+ @GetMapping("/listatodos/{id}")
     public ResponseEntity<ProductModel> listarPorId(@PathVariable(value = "id") long id){
         Optional<ProductModel> produto = productService.buscaPorId(id);
         if(!produto.isPresent()){
